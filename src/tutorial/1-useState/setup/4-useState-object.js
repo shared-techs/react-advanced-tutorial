cf07e0da606ad2748186f1a4a0e6aa5a37fa43cb
Peter Rackley <peter@shared-techs.com>
2022-07-26 10:52:01 -0400

    useState with Object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 57,
+    message: 'hello'
+  });
+
+  const changeMessage = () => {
+    setPerson({
+      ...person,
+      message: 'bye'
+    });
+  };
+
+  console.log(person);
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button type='button' className='btn' onClick={changeMessage}>Change Message</button>
+    </>
+  );
 };
 
 export default UseStateObject;
